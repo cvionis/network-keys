363c3b5b2507d86c4f7c8ae33cb913e771530798
Chance Vionis <cvionis03@gmail.com>
2022-09-25 20:58:13 -0400

    Add comments specifying outline for completing script

--- getwifi.py
+++ getwifi.py
@@ -1,1 +1,7 @@
 import os
+
+# Use 'netsh wlan show profile' and get all listed user profiles; print them out and let the user choose which one's
+# password should be displayed
+
+# Once user chooses from the list of ssid's, run 'netsh wlan export profile key=clear'. 
+# Open the xml file the info was saved to using with open(); get the key (pw) inside <keyMaterial> and print it
