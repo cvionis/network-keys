fd83290e87b6c42091444b251275210485aaecf2
Chance Vionis <cvionis03@gmail.com>
2022-09-26 15:27:03 -0400

    Remove unnecessary comment

--- getwifi.py
+++ getwifi.py
@@ -39,7 +39,6 @@ pw_file_name = (f"Wi-Fi-{profile_value}")
 with open(f"{cwd}\\profile_info\\{pw_file_name}.xml", "r") as f:
     wifi_pw_element = "".join([i for i in list(f.read().split("\n")) if "keyMaterial" in i])
 
-# Can probably do this more efficiently (like searching for 8 numbers and returning the first matching instance)
 wifi_pw = ''.join(re.findall(r'[0-9]', wifi_pw_element))
     
 print(f"{profile_value}'s password: {wifi_pw}")
