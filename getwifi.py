88682826e000b3891779122d58e09ca11fcf7af6
Chance Vionis <cvionis03@gmail.com>
2022-09-25 21:14:43 -0400

    Save output of 'netsh wlan show profile' to file named 'ssid_profiles.txt' inside project directory

--- getwifi.py
+++ getwifi.py
@@ -1,7 +1,16 @@
 import os
 
-# Use 'netsh wlan show profile' and get all listed user profiles; print them out and let the user choose which one's
-# password should be displayed
+cwd = os.getcwd()
+
+os.system(f"netsh wlan show profile > {cwd}/ssid_profiles.txt")
+
+
+
+# Save project path to variable; Run 'netsh wlan show profile' and direct output to new file in project path; 
+# get the list of ssid's from the file,
+# print them out and let the user choose which one's password should be displayed.
 
 # Once user chooses from the list of ssid's, run 'netsh wlan export profile key=clear'. 
-# Open the xml file the info was saved to using with open(); get the key (pw) inside <keyMaterial> and print it
+# Open the xml file that the info was saved to using 'with open()'; 
+# get the key (pw) inside <keyMaterial> and print it
+
