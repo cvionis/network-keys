0ba319ae3324227e13a88e286a00f64f73817fae
cvionis <cvionis03@gmail.com>
2022-12-23 05:05:12 -0500

    Edit wifi.py
    Remove redundant variable from loop creating profile dictionary, improve quality of error messages

--- wifi.py
+++ wifi.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import re
-import shlex
 import subprocess
 
 def main():
@@ -20,13 +19,11 @@ def main():
     # Get listed user profiles from 'file_split' list and save them to a new list
     profiles = ([(i.split(':'))[1].replace(" ", "") for i in file_split if 'All User Profile' in i])
 
-    # Add profiles to a dictionary, giving each an incremented key ('c') starting from 1
+    # Add profiles to a dictionary, giving each an incremented label starting from 1
     p_dict = {}
-    c = 1
 
     for i in range(len(profiles)):
-        p_dict.update({c:profiles[i]})
-        c+=1
+        p_dict.update({i+1:profiles[i]})
         i+=1
 
     # Print all profiles, use a loop to check selection input, and save input to 'profile_input'
@@ -46,12 +43,11 @@ def main():
         if (profile_input in p_dict):
             break
         else:
-            print("\nInvalid choice: try again")
+            print("\nInvalid choice: Choose a number from the list")
 
     profile_value = p_dict.get(profile_input)
 
     # Export file containing profile's password to current working directory, hide processing info
-
     subprocess.run(["netsh", "wlan", "export", "profile", f"name={profile_value}", "folder=.", "key=clear", "|", "@echo", "off"], shell=True)
 
     pw_file = (f"Wi-Fi-{profile_value}.xml")
@@ -62,7 +58,7 @@ def main():
 
     wifi_pw = ''.join(re.findall(r'[0-9]', wifi_pw_element))
      
-    print(f"\n{profile_value}'s password:\n------------\n| {wifi_pw} |\n------------")
+    print(f"\n{profile_value}'s password:\n\n{wifi_pw}")
 
     # Delete file containing info about selected profile
     subprocess.run(["rm", "-f", f"{pw_file}"])
